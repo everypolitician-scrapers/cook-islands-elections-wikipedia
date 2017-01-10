83400742bded46fe6479f2e0aae1f4ca3f858159
Tony Bowden <tony@tmtm.com>
2017-01-10 09:53:05 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'nokogiri'
 require 'open-uri'
@@ -28,40 +29,40 @@ def scrape_term(t)
       tds = tr.css('td')
       next if tds.any? { |td| td.attr('colspan') }
       data = {
-        name: tds[2].text.tidy,
-        wikiname: tds[2].xpath('.//a[not(@class="new")]/@title').text,
-        party: tds[1].text.tidy,
+        name:         tds[2].text.tidy,
+        wikiname:     tds[2].xpath('.//a[not(@class="new")]/@title').text,
+        party:        tds[1].text.tidy,
         constituency: constituency,
-        votes: tds[3].text.to_i,
-        term: t[:term],
+        votes:        tds[3].text.to_i,
+        term:         t[:term],
       }
       # https://en.wikipedia.org/wiki/Mitiaro_by-election_2014
       data[:votes] -= 1 if t[:term] == '14' && data[:name] == 'Tuakeu Tangatapoto'
       data
     end.compact.sort_by { |d| d[:votes] }.reverse.first
-    ScraperWiki.save_sqlite([:name, :term, :constituency], winner)
+    ScraperWiki.save_sqlite(%i(name term constituency), winner)
   end
 end
 
 terms = [
   {
-    term: 14,
-    year: 2014,
+    term:     14,
+    year:     2014,
     headline: 'By constituency',
-    source: 'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2014'
+    source:   'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2014',
   },
   {
-    term: 13,
-    year: 2010,
+    term:     13,
+    year:     2010,
     headline: 'Electorate Results',
-    source: 'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2010'
+    source:   'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2010',
   },
   {
-    term: 12,
-    year: 2006,
+    term:     12,
+    year:     2006,
     headline: 'Electorate Results',
-    source: 'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2006'
-  }
+    source:   'https://en.wikipedia.org/wiki/Cook_Islands_general_election,_2006',
+  },
 ]
 
 terms.each { |t| scrape_term(t) }
